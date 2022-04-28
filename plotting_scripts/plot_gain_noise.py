9c90abbb44c529c59011c2e748bec95853991016
Ilya Besedin <ilia.besedin@gmail.com>
2022-04-28 20:07:22 +0300

    modified:   plotting_scripts/JPA/plot_jpa_noise.py 	modified:   plotting_scripts/plot_gain_noise.py 	routines/gain_noise.py

--- plotting_scripts/plot_gain_noise.py
+++ plotting_scripts/plot_gain_noise.py
@@ -1,7 +1,7 @@
 from matplotlib.pyplot import *
 from numpy import *
 import pickle
-from qsweepy import gain_noise
+from anti_qsweepy.routines import gain_noise
 import scipy.constants as sc
 
 data = pickle.load(open('raw_data.pkl', 'rb'))
